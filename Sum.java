cdcc81078b1c27e8bc69c413abc74b88c3043a78
Tushang <pateltushang76300@gmail.com>
2025-02-26 23:34:35 +0530

    added message in sum.ajmjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjmjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        System.out.println(a+b);
+
+        System.out.println("The sum is "a + b);
     }
 }
